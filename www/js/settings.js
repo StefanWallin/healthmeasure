6428235e2d0fc8e39a5ccfb5ca5637afeb304bcc
Stefan Wallin <stefan.w@festiz.com>
2014-10-09 02:05:34 +0200

    Add file that decides which menu entries are active.

---
+++ www/js/settings.js
@@ -0,0 +1,12 @@
+document.ngapp.value("settings", { 
+	"app": {
+		"menu_active": {
+			"measure": true,
+			"weigh": false,
+			"exercise": false,
+			"report": true,
+			"settings": false,
+			"feedback": false
+		}
+	}
+});
