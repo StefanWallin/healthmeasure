18d2a9aa2687493dffc6aca302a0734ec2ccc74b
Stefan Wallin <stefan.w@festiz.com>
2014-11-13 22:59:13 +0100

    Update cordova libraries to version 3.7.0.

--- js/cordova_plugins.js
+++ js/cordova_plugins.js
@@ -1,7 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.apache.cordova.vibration": "0.3.11"
+}
 // BOTTOM OF METADATA
 });
